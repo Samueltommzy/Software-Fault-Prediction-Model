49e08d36e301fe1d2996f9daa75dc617c6db59b3
Samuel Ologunowa <samueloluwatommzy@gmail.com>
2022-07-21 10:08:27 +0100

    Refactor raw data restructure script

--- sfp_model/data_restructure.py
+++ sfp_model/data_restructure.py
@@ -1,46 +1,36 @@
-# Pseudo code
-
-#single arff file
-#1. For each line in the file, if line starts with @attribute
-  #readline to string and split string by space
-  #Push splittedValue[1] to the column title array
-
 from os import listdir, path
 import csv
 
-#retrieve all datasets from the raw_data directory
-raw_datasets = [dataset for dataset in listdir('./raw_data') if dataset.endswith('.arff')]
-print(raw_datasets)
+def arff_to_csv(file):
+  file_name = file.split('.')[0]
+  with open('./raw_data/'+file, 'r') as arff_file:
+    file_content = arff_file.readlines()
+    rows = []
+    columns = []
 
-#loop through the datasets and generate a csv file
-# for dataset in raw_datasets:
-#   with open(dataset, 'r') as file:
-#     file_content = file.readlines()
 
-with open('./raw_data/cm1.arff', 'r') as file:
-  file_content = file.readlines()
-  file_name, file_extension = path.splitext(file.name)
-  csv_data = []
-  header = []
-  data = False
-   
-  for content in file_content:
-     #append @data before the dataset values
-    if not content.startswith('@attribute'):
-      content = '@data' + ' ' +  content
-    if content.startswith('@attribute'):
-      column_name = content.split(' ')[1]
-      header.append(column_name)
-    elif "@data" in content:
-      csv_data.append(content.split(' ')[1])
-    else:
-      pass
-  # print(file_name, file_extension)
- 
-  final_data = [d.split(',') for d in csv_data]
-  # print(header,final_data)
-  with open(file_name+'.csv', 'w') as csv_output:
-    wr = csv.writer(csv_output)
-    wr.writerow(header)
-    wr.writerows(final_data)
-  csv_output.close()
+    for content in file_content:
+      #append @data before the dataset values which will serve as rows in the csv file
+      if not content.startswith('@attribute'):
+        content = '@data' + ' ' +  content
+      #attributes serve as column in the csv file
+      if content.startswith('@attribute'):
+        column_name = content.split(' ')[1]
+        columns.append(column_name)
+      elif content.startswith('@data'):
+        rows.append(content.split(' ')[1])
+      else:
+        pass
+    row_data = [row.split(',') for row in rows]
+    #write to a csv file in a new csv_data folder
+    with open('./csv_data/'+file_name+'.csv', 'w') as csv_output:
+      wr = csv.writer(csv_output)
+      wr.writerow(columns)
+      wr.writerows(row_data)
+    csv_output.close()
+
+if __name__ == "__main__":
+  #retrieve all datasets from the raw_data directory
+  raw_datasets = [dataset for dataset in listdir('./raw_data') if dataset.endswith('.arff')]
+  for file in raw_datasets:
+    arff_to_csv(file)
